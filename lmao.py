02696dff211e6bc055d08fe8fbf2c7a5035ee2a0
Hrishav <114722342+Hrishavvv@users.noreply.github.com>
2023-12-08 17:50:09 +0530

    Update lmao.py

--- lmao.py
+++ lmao.py
@@ -1,3 +1,4 @@
+from rich import print
 import requests
 
 def fetch_ip_details(ip_address):
@@ -7,21 +8,25 @@ def fetch_ip_details(ip_address):
     if response.status_code == 200:
         ip_details = response.json()
         for key, value in ip_details.items():
-            print(f"{key}: {value}")
+            if key == 'status':  # Checking for specific details like 'status'
+                print(f"[orange1]{key}[/orange1]: [cyan]{value}[/cyan]")  # Displaying 'status' in orange and its value in cyan
+            else:
+                print(f"[cyan]{key}[/cyan]: [cyan]{value}[/cyan]")  # Displaying all other details in cyan
     else:
-        print("Failed to fetch IP details. Please check the IP address and try again.")
+        print("[red]Failed to fetch IP details. Please check the IP address and try again.[/red]")  # Error message in red
 
-# Red-styled "LMAO" ASCII art banner
-print(r"""
-\033[91m   __         __    __     ______     ______    
- /\ \       /\ "-./  \   /\  __ \   /\  __ \   
- \ \ \____  \ \ \-./\ \  \ \  __ \  \ \ \/\ \  
-  \ \_____\  \ \_\ \ \_\  \ \_\ \_\  \ \_____\ 
-   \/_____/   \/_/  \/_/   \/_/\/_/   \/_____/ 
-\033[0m
-""")
+# Red-styled "LMAO" ASCII art banner using rich
+print(
+"""
+[red]   __         __    __     ______     ______    [/red]
+[red] /\ \       /\ "-./  \   /\  __ \   /\  __ \   [/red]
+[red] \ \ \____  \ \ \-./\ \  \ \  __ \  \ \ \/\ \  [/red]
+[red]  \ \_____\  \ \_\ \ \_\  \ \_\ \_\  \ \_____\ [/red]
+[red]   \/_____/   \/_/  \/_/   \/_/\/_/   \/_____/  [/red]
+"""
+)
 
-# Get user input for the IP address
-ip = input("\033[92mEnter the IP address: \033[0m")
+# Get user input for the IP address (in ANSI light green)
+ip = input("\033[92;1mEnter the IP address: \033[0m")
 fetch_ip_details(ip)
-        
+                      
